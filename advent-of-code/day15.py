24ed021fa474030a5fdcb220b80866d52eb93c23
malcops <paul.malcolm@protonmail.com>
2020-12-15 11:23:03 -0500

    working part 2?

--- advent-of-code/day15.py
+++ advent-of-code/day15.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 import pytest
 
+example1 = [0,3,6]
+example2 = [1,3,2]
+example3 = [2,1,3]
+example4 = [1,2,3]
+example5 = [2,3,1]
+example6 = [3,2,1]
+example7 = [3,1,2]
+input_list = [0,8,15,2,12,1,4]
+
+
+@pytest.mark.parametrize("test_input, expected", [
+                        (example1, 436),
+                        (example2, 1),
+                        (example3, 10),
+                        (example4, 27),
+                        (example5, 78),
+                        (example6, 438),
+                        (example7, 1836)
+                         ])
+def test_part_one(test_input, expected):
+
+    assert part_one(test_input) == expected
+
 
 def part_one(input_list):
 
     working = input_list[:]
-
     idx = len(input_list) + 1
     while idx < 2018:
+        print(idx)
         last = working[-1]
         if working.count(last) <= 1:
             working.append(0)
@@ -24,28 +47,54 @@ def part_one(input_list):
     return working[-1]
 
 
+@pytest.mark.parametrize("test_input, expected", [
+                        (example1, 436),
+                        (example2, 1),
+                        (example3, 10),
+                        (example4, 27),
+                        (example5, 78),
+                        (example6, 438),
+                        (example7, 1836)
+                        # (example1, 175594),
+                        # (example2, 2578),
+                        # (example3, 3544142),
+                        # (example4, 261214),
+                        # (example5, 6895259),
+                        # (example6, 18),
+                        # (example7, 362)
+                         ])
+def test_part_two(test_input, expected):
+
+    assert part_two(test_input) == expected
+
+
 def part_two(input_list):
 
-    pass
+    max_index = 2020# change to 30M
+    indexes = {x: "X" for x in range(0, max_index)}
+
+    # last item will be processed below
+    for idx, x in enumerate(input_list[:-1]):
+        indexes[x] = idx + 1
+
+
+    # e.g. if input_list has 3 items, we are starting turn 4
+    turn = len(input_list) + 1
+    last_added = input_list[-1]
+    while turn <= max_index:
+        if indexes[last_added] == "X":
+            next_number = 0
+        else:
+            next_number = turn - 1 - indexes[last_added]
+        indexes[last_added] = turn - 1
+        last_added = next_number
+        input_list.append(next_number)
+        turn += 1
+    print(input_list)
+    return last_added
 
 
 if __name__ == "__main__":
 
-    example1 = [0,3,6]
-    example2 = [1,3,2]
-    example3 = [2,1,3]
-    example4 = [1,2,3]
-    example5 = [2,3,1]
-    example6 = [3,2,1]
-    example7 = [3,1,2]
-    input_list = [0,8,15,2,12,1,4]
-
-    assert part_one(example1) == 436
-    assert part_one(example2) == 1
-    assert part_one(example3) == 10
-    assert part_one(example4) == 27
-    assert part_one(example5) == 78
-    assert part_one(example6) == 438
-    assert part_one(example7) == 1836
-    part_one(input_list)
-    part_two(input_list)
+    # part_one(input_list)
+    part_two(example1)
