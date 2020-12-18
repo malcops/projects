5226414a322e66ec68389c645106714d9b884273
malcops <paul.malcolm@protonmail.com>
2020-12-18 11:09:12 -0500

    Advent of Code - Day 18

---
+++ advent-of-code/day18.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+import pytest
+import re
+
+input_list = []
+with open('day18.txt', 'r') as f:
+    strings = f.read().splitlines()
+    for item in strings:
+        if item:
+            input_list.append(item)
+
+
+
+@pytest.mark.parametrize("expression, result", [
+                         ("1 + 2 * 3 + 4 * 5 + 6", 71),
+                         ])
+def test_evaluate_lr(expression, result):
+
+	assert evaluate_lr(expression) == result
+
+
+def evaluate_lr(expression):
+    OPERATION_RE = r"[0-9]+ [+*] [0-9]+"
+
+    while re.search(OPERATION_RE, expression):
+        x = re.search(OPERATION_RE, expression)
+        repl = str(eval(x.group(0)))
+        expression = re.sub(OPERATION_RE, repl, expression, 1)
+    return int(expression)
+
+
+@pytest.mark.parametrize("expression, result", [
+                         ("1 + 2 * 3 + 4 * 5 + 6", 71),
+                         ("1 + (2 * 3) + (4 * (5 + 6))", 51),
+                         ("2 * 3 + (4 * 5)", 26),
+                         ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437),
+                         ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240),
+                         ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)
+                         ])
+def test_evaluate(expression, result):
+
+	assert evaluate(expression) == result
+
+
+def evaluate(expression):
+
+    BRACKETS_RE = r"(\([0-9+* ]+\))"
+    while re.search(BRACKETS_RE, expression):
+        x = re.search(BRACKETS_RE, expression)
+        brackets = x.group(0)
+        inside_brackets = brackets[1:-1]
+        repl = str(evaluate_lr(inside_brackets))
+        expression = re.sub(BRACKETS_RE, repl, expression, 1)
+
+    OPERATION_RE = r"[0-9]+ [+*] [0-9]+"
+    while re.search(OPERATION_RE, expression):
+        x = re.search(OPERATION_RE, expression)
+        repl = str(eval(x.group(0)))
+        expression = re.sub(OPERATION_RE, repl, expression, 1)
+
+    return int(expression)
+
+
+def part_one(input_list):
+
+    count = 0
+    for exp in input_list:
+        count += evaluate(exp)
+
+    print("Part 1: ", count)
+
+
+def part_two(input_list):
+
+    pass
+
+
+if __name__ == "__main__":
+
+    part_one(input_list)
+    part_two(input_list)
