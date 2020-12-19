efb9f2861d9e1530edfa60f05033ddf75ba0ddfe
malcops <paul.malcolm@protonmail.com>
2020-12-19 03:30:58 -0500

    fixup! fixup! Working example, part one incorrect answer

--- advent-of-code/pday17.py
+++ advent-of-code/pday17.py
@@ -18,7 +18,10 @@ input1 = [
 
 
 def process_cube(cube):
-    
+
+    assert len(cube.shape) == 3
+    assert len(set(cube.shape)) == 1
+
     print(len(cube))
     start = 0
     stop = len(cube)
@@ -42,6 +45,32 @@ def process_cube(cube):
     
     return new_cube
 
+def process_4d(array):
+    assert len(array.shape) == 4
+    assert len(set(array.shape)) == 1   
+    start = 0
+    stop = len(array)
+    new_array = np.copy(array)
+    print(new_array)
+    for x in range(start, stop):
+        for y in range (start, stop):
+            for z in range(start, stop):
+                for w in range(start, stop):
+                    if array[x][y][z][w]:
+                        print(x,y,z,w)
+                        count = np.count_nonzero(array[x-1:x+2, y-1:y+2, z-1:z+2, w-1:w+2]) # -1 for the current cube
+                        print(count-1)
+                        if count-1 not in (2,3):
+                            print('turning off: ', x,y,z,w)
+                            new_array[x][y][z][w] = 0
+                    else:
+                        count = np.count_nonzero(array[x-1:x+2, y-1:y+2, z-1:z+2, w-1:w+2])
+                        if count == 3:
+                            print("turning on: ", x,y,z,w)
+                            new_array[x][y][z][w] = 1
+    
+    return new_array
+
 
 
 @pytest.mark.parametrize("input_array, output_dimensions", [
@@ -57,28 +86,30 @@ def test_grow_by_one(input_array, output_dimensions):
 
 
 def grow_by_one(array):
-    size_tuple = tuple(x+2 for x in array.shape)
-    bigger = np.full(size_tuple, fill_value='.')
     return np.pad(array, 1, mode='constant')
 
 
 def part_one(input_list):
     a = np.array(input_list, ndmin=3)
     a_slice = np.full_like(a, fill_value=0)
+    print(a.shape)
+    print(a_slice.shape)
     array = np.concatenate([a_slice, a, a_slice], 0)
+    print(array.shape)
+    array = np.concatenate([a_slice, array, a_slice], 0)
+    print(array.shape)
+    array = np.concatenate([a_slice, array, a_slice], 0)
+    print(array.shape)
+    array = np.concatenate([array, a_slice], 0)
+    print(array.shape)
     assert np.count_nonzero(array) == 24
     it = 0
-    array = grow_by_one(array)
-    array = grow_by_one(array)
-    array = grow_by_one(array)
-    array = grow_by_one(array)
     array = grow_by_one(array)
     array = process_cube(array)
     count = np.count_nonzero(array)
     print(count)
     assert np.count_nonzero(array) == 77
     it = 1
-
     array = grow_by_one(array)
     array = process_cube(array)
     count = np.count_nonzero(array)
@@ -94,16 +125,59 @@ def part_one(input_list):
         array = process_cube(array)
         it += 1  
     count = np.count_nonzero(array)
+    print(array.shape)
     print("Part1: ", count)
     return count
 
 
 def part_two(input_list):
+    a = np.array(input_list, ndmin=4)
+    a_slice = np.full_like(a, fill_value=0)
+    array = np.concatenate([a_slice, a, a_slice], 0)
+    print(array.shape)
 
-    pass
+    it = 0
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    array = grow_by_one(array)
+    print(array.shape)
+    return
+    array = process_4d(array)
+    count = np.count_nonzero(array)
+    print(count)
+    it = 1
+    array = grow_by_one(array)
+    array = process_cube(array)
+    count = np.count_nonzero(array)
+    print(count)
+    it = 2
+
+
+
+    while it != 6:
+        print("Iteration: ", it)
+        array = grow_by_one(array)
+        array = process_cube(array)
+        it += 1  
+    count = np.count_nonzero(array)
+    print(array.shape)
+    print("Part1: ", count)
+    return count
+    
 
 
 if __name__ == "__main__":
 
     part_one(input1)
-    part_two(input1)
+    # part_two(input1)
