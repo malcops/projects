69ac8b74748ac0b9a9c09af359c6469508cfeb83
malcops <paul.malcolm@protonmail.com>
2020-12-19 17:13:09 -0500

    working examples.. part 1 timeout

--- advent-of-code/day19.py
+++ advent-of-code/day19.py
@@ -10,19 +10,22 @@ with open('day19.txt', 'r') as f:
         if item:
             if ':' in item:
                 lhs = int(item.split(": ")[0])
-                rhs = item.split(": ")[1]
+                if '|' not in item:
+                    rhs = item.split(": ")[1].replace("\"", "")
+                else:
+                    rhs = "(" + item.split(": ")[1].replace("\"", "") + ")"
                 rules[lhs] = rhs
             else:
                 messages.append(item)
 
 print(rules)
-print(messages)
+# print(messages)
 
 example0_rules = {0: "1 2", 1: "a",
-                  2: "1 3 | 3 1", 3: "b"}
+                  2: "(1 3 | 3 1)", 3: "b"}
 
-example1_rules = {0: "4 1 5", 1: "2 3 | 3 2",
-                  2: "4 4 | 5 5", 3: "4 5 | 5 4",
+example1_rules = {0: "4 1 5", 1: "(2 3 | 3 2)",
+                  2: "(4 4 | 5 5)", 3: "(4 5 | 5 4)",
                   4: "a", 5: "b"}
 
 @pytest.mark.parametrize("rules, msg, validity", [
@@ -44,20 +47,30 @@ def test_check_message(rules, msg, validity):
 def check_message(rules, msg):
 
     rules_sub = dict(rules)
+    FINISHED_RE = re.compile(r"(^([ a-z|)(])+$)")
+    for idx, char in enumerate(rules_sub[0].split(" ")):
+        print(idx, char)
 
-    FINISHED_RE = r"(^([ a-z|)(])+$)" 
-    it = 0
-    while not re.match(FINISHED_RE, rules_sub[0]):
-    # while it < 5:
-        for idx, char in enumerate(rules_sub[0]):
-            if char.isnumeric():
-                if '|' in rules[int(char)]:
-                    str_to_add = "(" + rules[int(char)] + ")"
-                else:
-                    str_to_add = rules[int(char)]
-                rules_sub[0] = rules_sub[0].replace(char, str_to_add)
-                print(rules_sub[0])
-        it += 1
+    # (Pdb) print(rules[0])
+    # (29 104 | 115 95) (29 104 | 115 95) (95 7 | 104 130)
+    # (Pdb) step
+    # > /home/paul/BBB-projects/advent-of-code/day19.py(57)check_message()
+    # -> if char.isnumeric():
+    # (Pdb) print(idx)
+    # 0
+    # (Pdb) print(char)
+    # (29
+
+    print("starting rules: ", rules[0])
+    while not FINISHED_RE.match(rules[0]):
+    # while not re.match(FINISHED_RE, rules_sub[0]):
+        for char in set([x for x in re.findall(r"\b\d+\b", rules[0])]): # maybe can make this a set
+            if '|' in rules[int(char)]:
+                str_to_add = "(" + rules[int(char)] + ")"
+            else:
+                str_to_add = rules[int(char)]
+            rules_sub[0] = rules_sub[0].replace(char, str_to_add)
+        rules = rules_sub
 
     regex_string = rules_sub[0].replace(" ", "")
     print(regex_string)
@@ -66,11 +79,10 @@ def check_message(rules, msg):
 
     return bool(re.match(MESSAGE_RE, msg))
 
+
 def part_one(rules_dict, msg_list):
-    # number of messages that completely match rule 0
 
     check_message(rules_dict, msg_list[0])
-
     print("Part1: ", count)
 
 
