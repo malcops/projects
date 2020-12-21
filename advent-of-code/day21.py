c5886d911c856059e96e4d12928f0b62d6e08837
malcops <paul.malcolm@protonmail.com>
2020-12-21 09:51:53 -0500

    fixup! fixup! Advent of Code - Day 21

--- advent-of-code/day21.py
+++ advent-of-code/day21.py
@@ -62,8 +62,27 @@ def part_one(input_list):
     print(counts)
     print("Part1 :", sum(counts.values()) )
 
-def part_two(input_list):
-    pass
+
+    dangerous_ingredients = set(ingredients) ^ set(safe_ingredients)
+    print(dangerous_ingredients)
+    assigned = dict()
+    while len(assigned) < len(my_dict):
+        my_dict = dict()
+        for line in input_list:
+            ing = list(filter(None, line.split("(")[0].split(" ")))
+            allerg = list(filter(None, line.split("contains")[1].replace(")", "").replace(",","").split(" ")))
+            dangerous = list(set(dangerous_ingredients).intersection(ing))
+            for a in allerg:
+                if a not in my_dict:
+                    my_dict[a] = list(dangerous)
+                else:
+                    my_dict[a] = list(set(my_dict[a]).intersection(dangerous))
+        for a in my_dict:
+            if len(my_dict[a]) == 1:
+                assigned[a] = my_dict[a][0]
+                dangerous_ingredients.remove(my_dict[a][0])
+        print(assigned)
+
 
 
 if __name__ == "__main__":
@@ -73,7 +92,3 @@ if __name__ == "__main__":
     stop = time.time()
     print("time: ", stop - start)
 
-    start = time.time()
-    part_two(foods)
-    stop = time.time()
-    print("time: ", stop - start)
