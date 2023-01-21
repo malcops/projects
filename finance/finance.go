81384b368aa3db5d2c4ffa17881f59a7fd2cb461
Paul Malcolm <paul.malcolm@protonmail.com>
2023-01-20 19:02:51 -0500

    [UPDATE] finance.go handle errors when run offline

--- finance/finance.go
+++ finance/finance.go
@@ -23,7 +23,7 @@ func loadYaml(f string) map[string]int {
 	return data
 }
 
-func printMarketSummary() {
+func getMarketSummary() string {
 
 	// S&P500, S&P/TSX, CAD/USD, Bitcoin, ETH
 	marketInfo := [5]string{"^GSPC", "^GSPTSE", "CADUSD=X", "BTC-CAD", "ETH-CAD"}
@@ -31,42 +31,41 @@ func printMarketSummary() {
 	for _, ticker := range marketInfo {
 		dat, err := quote.Get(ticker)
 		if err != nil {
-			// Uh-oh!
-			panic(err)
+			return ""
 		}
 		output = fmt.Sprintf("%s\n%-10s $%10.2f %-20s", output, ticker, dat.RegularMarketPrice, dat.ShortName)
 	}
-	fmt.Println(output)
+	return output
 }
 
-func printHoldingsSummary(data map[string]int) {
+func getHoldingsSummary(data map[string]int) string {
 
 	total_value := 0.0
 	output := "\nHOLDINGS SUMMARY"
 	for holding, data := range data {
 		dat, err := quote.Get(holding)
 		if err != nil {
-			// Uh-oh!
-			panic(err)
+			return ""
 		}
 		value := dat.RegularMarketPrice * float64(data)
 		output = fmt.Sprintf("%s\n%d shares of %s: $%.2f", output, data, holding, value)
 
 		total_value += value
 	}
-	fmt.Println(output)
 
 	res := fmt.Sprintf("Total holdings: $%.2f", total_value)
-	fmt.Println(res)
+	return res
 }
 
 func main() {
-
-	printMarketSummary()
+	
+	dat := getMarketSummary()
+	fmt.Println(dat)
 
 	argsLength := len(os.Args)
 	if argsLength > 1 {
 		holdings := loadYaml(os.Args[1])
-		printHoldingsSummary(holdings)
+		dat := getHoldingsSummary(holdings)
+		fmt.Println(dat)
 	}
 }
